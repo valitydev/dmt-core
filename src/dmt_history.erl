19d8f57198f2cbe5b64aa4a923ba32774e505503
Aleksey Kashapov <nanodirijabl@gmail.com>
2024-05-14 13:44:13 +0300

    FIN-31: Adds support for snapshot creation timestamp (#7)
    * FIN-31: Adds support for snapshot creation timestamp
    
    * Bumps CI workflow
    
    * Temporarily disables codecov report upload; fixes protocol and dialyzer warnings
    
    * Fix possible bug for partial history backwards traversal

--- src/dmt_history.erl
+++ src/dmt_history.erl
@@ -12,7 +12,7 @@
 
 -spec head(history()) -> {ok, snapshot()} | {error, dmt_domain:operation_error()}.
 head(History) ->
-    head(History, #domain_conf_Snapshot{version = 0, domain = dmt_domain:new()}).
+    head(History, #domain_conf_Snapshot{version = 0, domain = dmt_domain:new(), created_at = undefined}).
 
 -spec head(history(), snapshot()) -> {ok, snapshot()} | {error, dmt_domain:operation_error()}.
 head(History, Snapshot) when map_size(History) =:= 0 ->
@@ -25,12 +25,13 @@ head(History, Snapshot) ->
 travel(To, _History, #domain_conf_Snapshot{version = From} = Snapshot) when To =:= From ->
     {ok, Snapshot};
 travel(To, History, #domain_conf_Snapshot{version = From, domain = Domain}) when To > From ->
-    #domain_conf_Commit{ops = Ops} = maps:get(From + 1, History),
+    #domain_conf_Commit{ops = Ops, created_at = CreatedAt} = maps:get(From + 1, History),
     case dmt_domain:apply_operations(Ops, Domain) of
         {ok, NewDomain} ->
             NextSnapshot = #domain_conf_Snapshot{
                 version = From + 1,
-                domain = NewDomain
+                domain = NewDomain,
+                created_at = CreatedAt
             },
             travel(To, History, NextSnapshot);
         {error, _} = Error ->
@@ -38,11 +39,20 @@ travel(To, History, #domain_conf_Snapshot{version = From, domain = Domain}) when
     end;
 travel(To, History, #domain_conf_Snapshot{version = From, domain = Domain}) when To < From ->
     #domain_conf_Commit{ops = Ops} = maps:get(From, History),
+    %% NOTE In case of backwards traversal of partial history of
+    %%      commits there may be no 'previous' commit. Use 'undefined'
+    %%      for creation timestamp as a fallback value.
+    CreatedAt =
+        case maps:get(From - 1, History, undefined) of
+            #domain_conf_Commit{created_at = T} -> T;
+            undefined -> undefined
+        end,
     case dmt_domain:revert_operations(Ops, Domain) of
         {ok, NewDomain} ->
             PreviousSnapshot = #domain_conf_Snapshot{
                 version = From - 1,
-                domain = NewDomain
+                domain = NewDomain,
+                created_at = CreatedAt
             },
             travel(To, History, PreviousSnapshot);
         {error, _} = Error ->
